a0ebdca74c3b598ec0e88497e1786bd752eccffd
smbmunna <smbmunna@gmail.com>
2026-07-12 17:55:18 +0600

    setup tanstack query

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,16 @@ import "./index.css";
 import App from "./App.jsx";
 import { RouterProvider } from "react-router/dom";
 import { router } from "./router/router.jsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router}>
-      <App />
-    </RouterProvider>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router}>
+        <App />
+      </RouterProvider>
+    </QueryClientProvider>
   </StrictMode>,
 );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,53 @@
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
 
-export default function Home(){
-
-    return (
-        <div>Home</div>
-    )
+export default function AllProducts() {
+  const axiosSecure = useAxiosSecure();
+  const fetchItems = async () => {
+    const response = await axiosSecure("/allItems");
+    //console.log(response.data);
+    return response.data;
+  };
+  const { data: items = [], refetch } = useQuery({
+    queryKey: ["sneakerItems"],
+    queryFn: fetchItems,
+  });
+  return (
+    <div>
+      <h1>Total Products:{items.length}</h1>
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>Item Image</th>
+              <th>Item Code</th>
+              <th>Item Name</th>
+              <th>Price</th>
+            </tr>
+          </thead>
+          <tbody>
+            {items.map((item) => (
+              <tr key={item.id}>
+                <td>
+                  {" "}
+                  <div className="avatar">
+                    <div className="mask mask-squircle h-12 w-12">
+                      <img
+                        src={item.image}
+                        alt="Avatar Tailwind CSS Component"
+                      />
+                    </div>
+                  </div>
+                </td>
+                <td>{item.item_code}</td>
+                <td>{item.item_name}</td>
+                <td>{item.price}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
